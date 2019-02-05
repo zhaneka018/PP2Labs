2901185239e074901e7a3f0e64782ec685cf3971
Zhanerke <zhanerke.tattibek@bk.ru>
2019-02-05 14:01:18 +0600

    Update

---
+++ Lab1 Task1/Lab1 Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                
+            }
+        }
+    }
+}
