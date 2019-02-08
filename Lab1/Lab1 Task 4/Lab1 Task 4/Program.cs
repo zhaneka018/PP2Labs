a79e608d2acd0b81054f4dc6af395aaece30eaf5
Zhanerke <zhanerke.tattibek@bk.ru>
2019-02-08 12:22:42 +0600

    neka

---
+++ Lab1/Lab1 Task 4/Lab1 Task 4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "[*]";
+            int n;
+            string a = Console.ReadLine();
+            n = int.Parse(a);
+            int check = 1;
+            for(int i=0; i<n; i++)
+            {
+                for(int j = 0; j < check; j++)
+                {
+                    Console.Write(str);
+                }
+                check++;
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
