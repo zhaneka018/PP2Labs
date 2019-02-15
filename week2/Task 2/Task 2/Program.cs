7c97cb96d944b58d7d3b9b28bbdeaa7cccbb8559
Zhanerke <zhanerke.tattibek@bk.ru>
2019-02-16 00:16:46 +0600

    addedLab2Task2

--- week2/Task 2/Task 2/Program.cs
+++ week2/Task 2/Task 2/Program.cs
@@ -13,13 +13,13 @@ namespace Task_2
         {
             FileStream CreateFile = new FileStream(@"C:\Users\Zhanerke\Desktop\PP2Labs\outputPrime.txt", FileMode.Create, FileAccess.Write);
             StreamWriter WriteInFile = new StreamWriter(CreateFile); // Благодаря StreamWriter мы можем писать в файле, который создали через FileStream
-            string str = File.ReadAllText(@"C:\Users\Zhanerke\Desktop\PP2Labs\inputprime.txt");
+            string str = File.ReadAllText(@"C:\Users\Zhanerke\Desktop\PP2Labs\inputprime.txt"); //Читаем инпутфайл и сохраняем его в стринг
             string[] massiveStr = str.Split();
             int size = massiveStr.Count();
             for (int i = 0; i < size; i++)
             {
-                int num = int.Parse(massiveStr[i]);
-                int count = 0;
+                int num = int.Parse(massiveStr[i]);//Каждое число в стриннге записываем как интейджер
+                int count = 0; // Каунтер для считывания делителей
                 for(int j = 1; j <= num; j++)
                 {
                     if(num % j == 0)
@@ -27,9 +27,9 @@ namespace Task_2
                         count++;
                     }
                 }
-                if(count == 2 && num != 1)
+                if(count == 2 && num != 1) // Это основное условие для прайм чисел
                 {
-                    WriteInFile.Write(num + " ");
+                    WriteInFile.Write(num + " ");//выводим аутпут
                 }
             }
             WriteInFile.Close();
