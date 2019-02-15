717a8048e459b7565bd4b95e0a0b071d960f8b2c
Zhanerke <zhanerke.tattibek@bk.ru>
2019-02-16 02:04:34 +0600

    addedLab2Task3

--- week2/Task 3/Task 3/Program.cs
+++ week2/Task 3/Task 3/Program.cs
@@ -9,28 +9,28 @@ namespace Task_3
 {
     class Program
     {
-        public static void Probely(int lvl)             //функция для вывода имени каждого файла и директори
+        public static void Probely(int lvl)             //функция для вывода названия каждого файла и directory
         {
             for (int i = 0; i < lvl; i++)
             {
                 Console.Write("    ");
             }
         }
-        public static void Direc(DirectoryInfo dir, int lvl)        //функция для вывода имени каждого файла и директори 
+        public static void Direc(DirectoryInfo dir, int lvl)        //функция для вывода названия каждого файла и directory 
         {
-            foreach (FileInfo f in dir.GetFiles())      //взять файлы из директори и показать
+            foreach (FileInfo f in dir.GetFiles())      //взять файлы из directory и показать
             {
                 Probely(lvl);
                 Console.ForegroundColor = ConsoleColor.Blue;
 
                 Console.WriteLine(f.Name);
             }
-            foreach (DirectoryInfo d in dir.GetDirectories())  //взять директориес из директори и показать
+            foreach (DirectoryInfo d in dir.GetDirectories())  //взять directories из directory и показать
             {
                 Probely(lvl);
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine(d.Name);
-                Direc(d, lvl + 1);        //путем рекурсии вызовим метод Direc, чтобы показать другие файлы и каталоги
+                Direc(d, lvl + 1);        //путем рекурсии вызываем метод Direc, чтобы показать другие файлы и каталоги
             }
 
         }
