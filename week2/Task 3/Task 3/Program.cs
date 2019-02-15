4fbb042898b56435e47a69e719101103d4247d2a
Zhanerke <zhanerke.tattibek@bk.ru>
2019-02-16 01:53:16 +0600

    addedLab2Task3

---
+++ week2/Task 3/Task 3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_3
+{
+    class Program
+    {
+        public static void Probely(int lvl)             //функция для вывода имени каждого файла и директори
+        {
+            for (int i = 0; i < lvl; i++)
+            {
+                Console.Write("    ");
+            }
+        }
+        public static void Direc(DirectoryInfo dir, int lvl)        //функция для вывода имени каждого файла и директори 
+        {
+            foreach (FileInfo f in dir.GetFiles())      //взять файлы из директори и показать
+            {
+                Probely(lvl);
+                Console.ForegroundColor = ConsoleColor.Blue;
+
+                Console.WriteLine(f.Name);
+            }
+            foreach (DirectoryInfo d in dir.GetDirectories())  //взять директориес из директори и показать
+            {
+                Probely(lvl);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(d.Name);
+                Direc(d, lvl + 1);        //путем рекурсии вызовим метод Direc, чтобы показать другие файлы и каталоги
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+            DirectoryInfo dirr = new DirectoryInfo(path);
+            Direc(dirr, 0);
+            Console.ReadKey();
+        }
+    }
+}
+
